623d6d65ef73a56473bb3a874e3ec416f6540e8b
JayLiuTU <tuj76761@temple.edu>
2024-02-06 18:46:03 -0500

    render hand lines and hand origin

--- RenderHands.py
+++ RenderHands.py
@@ -1,59 +1,68 @@
 import pygame
 
-class RenderHands():
+
+class RenderHands:
 
     def __init__(self, surface, hand_scale) -> None:
         self.surface = surface
         self.hand_scale = hand_scale
         self.font = pygame.font.Font("freesansbold.ttf", 30)
 
-    def connections(self,landmarks):
+    def connections(self, landmarks, mode):
+
         xy = []
 
         w, h = self.surface.get_size()
 
         for hand in landmarks:
             for point in hand:
-                xy.append(((point.x* self.hand_scale + 0.5)*w, (point.y* self.hand_scale+0.5)*h))
-                
-        #thumb
-        self.render_lines(xy[0], xy[1])
-        self.render_lines(xy[1], xy[2])
-        self.render_lines(xy[2], xy[3])
-        self.render_lines(xy[3], xy[4])
+                if mode:
+                    xy.append(
+                        (
+                            (point.x * self.hand_scale + 0.5) * w,
+                            (point.y * self.hand_scale + 0.5) * h,
+                        )
+                    )
+                else:
+                    xy.append(
+                        (
+                            point.x * w,
+                            point.y * h,
+                        )
+                    )
 
-        #index
-        self.render_lines(xy[0], xy[5])
-        self.render_lines(xy[5], xy[6])
-        self.render_lines(xy[6], xy[7])
-        self.render_lines(xy[7], xy[8])
+        # thumb
+        self.render_line(xy[0], xy[1])
+        self.render_line(xy[1], xy[2])
+        self.render_line(xy[2], xy[3])
+        self.render_line(xy[3], xy[4])
+        # index
+        self.render_line(xy[0], xy[5])
+        self.render_line(xy[5], xy[6])
+        self.render_line(xy[6], xy[7])
+        self.render_line(xy[7], xy[8])
+        # middle
+        self.render_line(xy[9], xy[10])
+        self.render_line(xy[10], xy[11])
+        self.render_line(xy[11], xy[12])
+        # ring
+        self.render_line(xy[13], xy[14])
+        self.render_line(xy[14], xy[15])
+        self.render_line(xy[15], xy[16])
+        # pinky
+        self.render_line(xy[0], xy[17])
+        self.render_line(xy[17], xy[18])
+        self.render_line(xy[18], xy[19])
+        self.render_line(xy[19], xy[20])
+        # knuckle
+        self.render_line(xy[5], xy[9])
+        self.render_line(xy[9], xy[13])
+        self.render_line(xy[13], xy[17])
 
-        #middle
-        self.render_lines(xy[9], xy[10])
-        self.render_lines(xy[10], xy[11])
-        self.render_lines(xy[11], xy[12])
+    def render_line(self, start, end):
+        pygame.draw.line(self.surface, (255, 255, 255), start, end, 5)
 
-        #ring
-        self.render_lines(xy[13], xy[14])
-        self.render_lines(xy[14], xy[15])
-        self.render_lines(xy[15], xy[16])
-
-        #pinky
-        self.render_lines(xy[0], xy[17])
-        self.render_lines(xy[17], xy[18])
-        self.render_lines(xy[18], xy[19])
-        self.render_lines(xy[19], xy[20])
-
-        #knuckle
-        self.render_lines(xy[5], xy[9])
-        self.render_lines(xy[9], xy[13])
-        self.render_lines(xy[13], xy[17])
-
-    def render_lines(self, start, end):
-            
-        pygame.draw.line(self.surface, (255,255,255), start, end, 5)
-
-    def render_hands(self, result, output_image, delay_ms, surface, mode):
+    def render_hands(self, result, output_image, delay_ms, surface, mode, origins):
         """Used as function callback by Mediapipe hands model
 
         This seems backwards, but:
@@ -69,33 +78,42 @@ class RenderHands():
         # print(delay_ms)
 
         surface.fill((0, 0, 0))
-        if result:
+        w, h = surface.get_size()
+        if result.handedness != []:
             if mode[0]:
                 hand_points = result.hand_world_landmarks
-                w, h = surface.get_size()
-                pygame.draw.circle(surface, (255,123,123), (0.5* w, 0.5 * h), 5)
-                
+                pygame.draw.circle(surface, (255, 0, 255), (0.5 * w, 0.5 * h), 5)
+
             else:
                 hand_points = result.hand_landmarks
 
+            self.connections(hand_points, mode[0])
             if hand_points:
                 # define colors for different hands
-
-                self.connections(hand_points)
 
                 hand_color = 0
                 colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 255, 255)]
                 # get every hand detected
-                for hand in hand_points:
+                for index, hand in enumerate(hand_points):
                     # each hand has 21 landmarks
+                    pygame.draw.circle(
+                        surface,
+                        (255, 0, 255),
+                        (origins[index][0] * w, origins[index][1] * h),
+                        5,
+                    )
                     for landmark in hand:
                         self.render_hands_pygame(
-                            colors[hand_color], landmark.x, landmark.y, surface, delay_ms, mode[0]
+                            colors[hand_color],
+                            landmark.x,
+                            landmark.y,
+                            surface,
+                            delay_ms,
+                            mode[0],
                         )
                     hand_color += 1
 
-
-    def render_hands_pygame(self,color, x, y, surface, delay_ms, mode):
+    def render_hands_pygame(self, color, x, y, surface, delay_ms, mode):
 
         w, h = self.surface.get_size()
 
@@ -104,13 +122,13 @@ class RenderHands():
             y *= self.hand_scale
             x += 0.5
             y += 0.5
-            
 
         pygame.draw.circle(surface, color, (x * w, y * h), 5)
-        delay_cam = self.font.render(str(round(delay_ms[0], 1)) + "ms", False, (255, 255, 255))
-        delay_AI = self.font.render(str(round(delay_ms[1], 1)) + "ms", False, (255, 255, 255))
+        delay_cam = self.font.render(
+            str(round(delay_ms[0], 1)) + "ms", False, (255, 255, 255)
+        )
+        delay_AI = self.font.render(
+            str(round(delay_ms[1], 1)) + "ms", False, (255, 255, 255)
+        )
         surface.blit(delay_cam, (0, 30))
         surface.blit(delay_AI, (0, 60))
-
-            
-
