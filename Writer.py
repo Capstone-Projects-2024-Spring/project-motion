d1444f71c4ecc1c93f2444227654f3a281fcb22d
JayLiuTU <tuj76761@temple.edu>
2024-02-15 20:18:11 -0500

    creates a csv file to contain the training dataset

--- Writer.py
+++ Writer.py
@@ -1,5 +1,5 @@
 import csv
-import os.path
+import time
 
 
 class Writer:
@@ -8,37 +8,34 @@ class Writer:
         self.gesture_list = gesture_list
         self.data_file = None
         self.writer = None
-        #create file and write gesture list
-        if not os.path.isfile("training_data.csv"):
-            self.data_file = open(
-                "training_data.csv", "a", newline="", encoding="utf-8"
-            )
-            self.writer = csv.writer(self.data_file)
-            self.writer.writerow(self.data_file, self.gesture_list)
-        else:
-            self.data_file = open(
-                "training_data.csv", "a", newline="", encoding="utf-8"
-            )
-            self.writer = csv.writer(self.data_file)
+        # create file and write gesture list
+
+        filename = str(round(time.time(),0)) + ".csv"
+
+        self.data_file = open(filename, "a", newline="", encoding="utf-8")
+        self.writer = csv.writer(self.data_file)
+        self.writer.writerow(self.gesture_list)
 
 
-    def write(self, data, velocity, gesture_index):
-        if data != []:
+    def write(self, data, velocity, gesture_vector):
+
+        if data != [] and gesture_vector != None:
             # add one-hot encoded gesture label
             row = []
-            for index, gesture in enumerate(self.gesture_list):
-                row.append("0")
-            
+            for index, gesture in enumerate(gesture_vector):
+                if gesture_vector[index] == "0" or gesture_vector[index] == "1":
+                    row.append(gesture_vector[index])
+
             hand = 0
             # add 21 landmarks
             for landmark in data[hand]:
-                row.append('{:.25f}'.format(landmark.x))
-                row.append('{:.25f}'.format(landmark.y))
-                row.append('{:.25f}'.format(landmark.z))
+                row.append("{:.25f}".format(landmark.x))
+                row.append("{:.25f}".format(landmark.y))
+                row.append("{:.25f}".format(landmark.z))
 
             # add 2D velocity vector
-            
-            row.append('{:.25f}'.format(velocity[hand][0]))
-            row.append('{:.25f}'.format(velocity[hand][1]))
+
+            row.append("{:.25f}".format(velocity[hand][0]))
+            row.append("{:.25f}".format(velocity[hand][1]))
 
             self.writer.writerow(row)
