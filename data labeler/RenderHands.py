73b91abe4768c833b488a557aa424c2ae7c5f1c4
JayLiuTU <tuj76761@temple.edu>
2024-03-27 16:43:05 -0400

    added data labeler branch files

---
+++ data labeler/RenderHands.py
@@ -0,0 +1,102 @@
+import pygame
+
+
+class RenderHands:
+    """Given the Mediapipe hands output data, renders the hands in a normilzed view or camera perspective view"""
+
+    def __init__(self, surface, hand_scale) -> None:
+        self.surface = surface
+        self.hand_scale = float(hand_scale)
+        self.font = pygame.font.Font("freesansbold.ttf", 30)
+
+    def connections(self, landmarks):
+
+        xy = []
+
+        w, h = self.surface.get_size()
+
+        for index,point in enumerate(landmarks):
+            xy.append(
+                (
+                    (float(point.x) * float(self.hand_scale) + 0.5) * w,
+                    (float(point.y) * float(self.hand_scale) + 0.5) * h,
+                )
+            )
+            if index == 20:
+                break
+
+
+        # thumb
+        self.render_line(xy[0], xy[1])
+        self.render_line(xy[1], xy[2])
+        self.render_line(xy[2], xy[3])
+        self.render_line(xy[3], xy[4])
+        # index
+        self.render_line(xy[0], xy[5])
+        self.render_line(xy[5], xy[6])
+        self.render_line(xy[6], xy[7])
+        self.render_line(xy[7], xy[8])
+        # middle
+        self.render_line(xy[9], xy[10])
+        self.render_line(xy[10], xy[11])
+        self.render_line(xy[11], xy[12])
+        # ring
+        self.render_line(xy[13], xy[14])
+        self.render_line(xy[14], xy[15])
+        self.render_line(xy[15], xy[16])
+        # pinky
+        self.render_line(xy[0], xy[17])
+        self.render_line(xy[17], xy[18])
+        self.render_line(xy[18], xy[19])
+        self.render_line(xy[19], xy[20])
+        # knuckle
+        self.render_line(xy[5], xy[9])
+        self.render_line(xy[9], xy[13])
+        self.render_line(xy[13], xy[17])
+
+    def render_line(self, start, end):
+        pygame.draw.line(self.surface, (255, 255, 255), start, end, 5)
+
+    def render_hands(
+        self, result
+    ):
+        
+        self.surface.fill((0, 0, 0))
+
+        w, h = self.surface.get_size()
+
+        hand_points = result
+
+        pygame.draw.circle(self.surface, (255, 0, 255), (0.5 * w, 0.5 * h), 5)
+        pygame.draw.line(
+            self.surface,
+            (255, 255, 0),
+            ((hand_points[21] + 0.5) * w, (hand_points[22] + 0.5) * h),
+            ((0.5) * w, (0.5) * h),
+            3,
+        )
+
+        self.connections(hand_points)
+        if hand_points:
+            # define colors for different hands
+
+            hand_color = 0
+            colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 255, 255)]
+            # get every hand detected
+            for index,landmark in enumerate(hand_points):
+                self.render_hands_pygame(
+                    colors[hand_color],
+                    float(landmark.x),
+                    float(landmark.y),
+                )
+                if index == 20:
+                    break
+
+    def render_hands_pygame(self, color, x, y):
+        w, h = self.surface.get_size()
+        x *= self.hand_scale
+        y *= self.hand_scale
+        x += 0.5
+        y += 0.5
+        pygame.draw.circle(self.surface, color, (x * w, y * h), 5)
+
---
+++ data labeler/Writer.py
@@ -0,0 +1,77 @@
+import csv
+class Writer:
+    """Generates a csv file containing a one hot encoded label of gestures and hand landmark point data"""
+
+    def __init__(self, gesture_list, filename, write_labels=False) -> None:
+        """Generates a random name for the csv file and initilizes writing
+
+        Args:
+            gesture_list (_type_): List of recognized gestures to write to file. The last index in the gesture list shall be a flag for determining if data needs to be written or not
+        """
+        self.gesture_list = gesture_list
+        self.data_file = None
+        self.writer = None
+        self.write_labels = write_labels
+        self.rows = 0
+        # create file and write gesture list
+
+        self.filename = "LABELED"+filename
+        try:
+            print("trying")
+            open(self.filename, "x")
+            self.data_file = open(self.filename, "a", newline="", encoding="utf-8")
+            self.writer = csv.writer(self.data_file)
+            if self.write_labels:
+                self.writer.writerow(self.gesture_list)
+        except:
+            with open(self.filename, "r", newline="", encoding="utf-8") as file:
+                self.data_file = file
+                data = list(csv.reader(self.data_file))
+                self.rows = len(data)-1
+            # Write the updated data back to the CSV file
+            self.data_file = open(self.filename, "a", newline="", encoding="utf-8")
+            self.writer = csv.writer(self.data_file)
+
+
+    def write(self, data, gesture_vector):
+
+        velocity = (data[21], data[22])
+
+        if data != [] and gesture_vector != None:
+            # add one-hot encoded gesture label
+            row = []
+
+            if self.write_labels:
+                for index, gesture in enumerate(gesture_vector):
+                    if gesture_vector[index] == "0" or gesture_vector[index] == "1":
+                        row.append(gesture_vector[index])
+            # add 21 landmarks
+            for index, landmark in enumerate(data):
+                # size is float32
+                row.append("{:.7f}".format(landmark.x))
+                row.append("{:.7f}".format(landmark.y))
+                row.append("{:.7f}".format(landmark.z))
+                if index == 20:
+                    break
+
+            # add 2D velocity vector
+
+            row.append("{:.7f}".format(velocity[0]))
+            row.append("{:.7f}".format(velocity[1]))
+            self.writer.writerow(row)
+            self.data_file.flush()
+
+    def remove_rows(self, num_of_rows_to_remove, current_frame):
+        self.data_file.close()
+        with open(self.filename, "r", newline="", encoding="utf-8") as file:
+            self.data_file = file
+            data = list(csv.reader(self.data_file))
+            del data[0]
+            for index in range(num_of_rows_to_remove):
+                del data[current_frame - index-1]
+
+        # Write the updated data back to the CSV file
+        self.data_file = open(self.filename, "w", newline="", encoding="utf-8")
+        self.writer = csv.writer(self.data_file)
+        self.writer.writerow(self.gesture_list)
+        self.writer.writerows(data)
