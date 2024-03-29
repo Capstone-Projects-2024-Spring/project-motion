2b50b2a1a5c5f4043f045f15dde1b094eaee47a6
gia-m12 <tul55523@temple.edu>
2024-03-28 20:12:31 -0400

    adding infinite scrolling to game

--- platformerGame/platformer.py
+++ platformerGame/platformer.py
@@ -19,6 +19,10 @@ FPS = 60
 # game variables
 GRAVITY = 1
 MAX_PLATFORMS = 10
+SCROLL_THRESH = 200
+scroll = 0
+bg_scroll = 0
+
 
 # define colors
 WHITE = (255, 255, 255)
@@ -27,6 +31,12 @@ WHITE = (255, 255, 255)
 bg_image = pygame.image.load('platformerGame/assets/bg.png').convert_alpha()
 jumpy_image = pygame.image.load('platformerGame/assets/jump.png').convert_alpha()
 platform_image = pygame.image.load('platformerGame/assets/wood.png').convert_alpha()
+
+# function for drawing the background
+def draw_bg(bg_scroll):
+    screen.blit(bg_image, (0,0 + bg_scroll))
+    screen.blit(bg_image, (0, -600 + bg_scroll))
+
 
 # player class
 class Player():
@@ -43,6 +53,7 @@ class Player():
         # reset variables
         dx = 0
         dy = 0
+        scroll = 0
         # process key presses
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
@@ -78,10 +89,17 @@ class Player():
             dy = 0
             self.vel_y = -20
 
+        # check if the player has bounced to the top of the screen
+        if self.rect.top <= SCROLL_THRESH:
+            # if player is jumping
+            if self.vel_y < 0:
+                scroll = -dy
+
         # update position
         self.rect.x += dx
-        self.rect.y += dy
+        self.rect.y += dy + scroll 
 
+        return scroll
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), (self.rect.x - 12, self.rect.y - 5))
@@ -95,6 +113,10 @@ class Platform(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+    
+    def update(self, scroll):
+        # update platforms vertical position
+        self.rect.y += scroll
 
 
 # player instance
@@ -106,8 +128,8 @@ platform_group = pygame.sprite.Group()
 #create temp platforms
 for p in range(MAX_PLATFORMS):
     p_w = random.randint(40, 60)
-    p_x = random.randint(0, SCREEN_WIDTH - p_w)
-    p_y = p * random.randint(80, 120)
+    p_x = random.randint(15, SCREEN_WIDTH - p_w - 10)
+    p_y = p * random.randint(80, 100)
     platform = Platform(p_x, p_y, p_w)
     platform_group.add(platform)
 
@@ -117,10 +139,19 @@ while run:
 
     clock.tick(FPS)
 
-    jumpy.move()
+    scroll = jumpy.move()
 
     # draw background
-    screen.blit(bg_image, (0,0))
+    bg_scroll += scroll
+    if bg_scroll >= 600:
+        bg_scroll = 0
+    draw_bg(bg_scroll)
+
+    # draw temp scroll threshold
+    pygame.draw.line(screen, WHITE, (0, SCROLL_THRESH), (SCREEN_WIDTH, SCROLL_THRESH))
+
+    # update platforms
+    platform_group.update(scroll)
 
     # draw characters
     platform_group.draw(screen)
