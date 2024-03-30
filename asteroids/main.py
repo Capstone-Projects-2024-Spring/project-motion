0272e2136dc1a3c94579bda09f660c4a439f9349
Zaina <tun35209@temple.edu>
2024-03-30 17:51:32 -0400

    added player image to the screen and can move/rotate it with arrow keys

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,4 +1,7 @@
+import math
 import pygame
+
+pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
@@ -20,18 +23,75 @@ run = True
 clock = pygame.time.Clock()
 game_over = False
 
+class Player(object):
+     def __init__(self):
+         self.img = player
+         self.width = self.img.get_width()
+         self.height = self.img.get_height()
+         self.x = SCREEN_WIDTH//2 
+         self.y = SCREEN_HEIGHT//2 
+         self.angle = 0
+         self.rotated_surface = pygame.transform.rotate(self.img, self.angle)
+         self.rotated_rect = self.rotated_surface.get_rect()
+         self.rotated_rect.center = (self.x,self.y)
+         self.cosine = math.cos(math.radians(self.angle + 90))
+         self.sine = math.sin(math.radians(self.angle + 90))
+         self.head = (self.x + self.cosine * self.width//2, self.y - self.sine * self.height//2)
+
+     def draw(self,window):
+          window.blit(self.rotated_surface,self.rotated_rect)
+
+     def turn_left(self):
+          self.angle +=5
+          self.rotated_surface = pygame.transform.rotate(self.img, self.angle)
+          self.rotated_rect = self.rotated_surface.get_rect()
+          self.rotated_rect.center = (self.x,self.y)
+          self.cosine = math.cos(math.radians(self.angle + 90))
+          self.sine = math.sin(math.radians(self.angle +90))
+          self.head = (self.x + self.cosine + self.width//2, self.y - self.sine * self.height/2)
+     
+     def turn_right(self):
+          self.angle -=5
+          self.rotated_surface = pygame.transform.rotate(self.img, self.angle)
+          self.rotated_rect = self.rotated_surface.get_rect()
+          self.rotated_rect.center = (self.x,self.y)
+          self.cosine = math.cos(math.radians(self.angle + 90))
+          self.sine = math.sin(math.radians(self.angle +90))
+          self.head = (self.x + self.cosine + self.width//2, self.y - self.sine * self.height/2)     
+
+     def move_forward(self):
+        self.x += self.cosine * 6
+        self.y -= self.sine * 6
+        self.rotated_surface = pygame.transform.rotate(self.img, self.angle)
+        self.rotated_rect = self.rotated_surface.get_rect()
+        self.rotated_rect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.width // 2, self.y - self.sine * self.height // 2)
+
+              
+
 def redraw_window():
         window.blit(bg,(0,0))
+        player.draw(window)
 
         pygame.display.update()
 
+player = Player()
 # game loop
 while run:
     clock.tick(60)
     if not game_over:   
-        redraw_window() 
+         keys = pygame.key.get_pressed()
+         if keys[pygame.K_LEFT]:
+                  player.turn_left()
+         if keys[pygame.K_RIGHT]:
+              player.turn_right()
+         if keys[pygame.K_UP]:  
+              player.move_forward()
+                    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    redraw_window()
 pygame.quit()
