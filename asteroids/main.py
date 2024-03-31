2879359641c049ffd90f2b4cbb66f8484b460363
Zaina <tun35209@temple.edu>
2024-03-31 18:37:03 -0400

    fixed shooting bug and implemented a method so if the ship goes off the screen it will come back on the opposite side but in same direction

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,7 +1,7 @@
 import math
 import pygame
 
-pygame.init()
+#pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
@@ -9,7 +9,7 @@ SCREEN_HEIGHT = 800
 # images used for game
 bg = pygame.image.load('asteroidpics/starbg.webp')
 alien = pygame.image.load('asteroidpics/alienShip.png')
-player_rocket = pygame.image.load('asteroidpics/spaceRocket.png')
+playerShip = pygame.image.load('asteroidpics/spaceRocket.png')
 star = pygame.image.load('asteroidpics/star.png')
 asteroid1 = pygame.image.load('asteroidpics/asteroid1.png')
 asteroid2 = pygame.image.load('asteroidpics/asteroid2.png')
@@ -25,7 +25,7 @@ game_over = False
 
 class Player(object):
      def __init__(self):
-         self.img = player_rocket
+         self.img = playerShip
          self.width = self.img.get_width()
          self.height = self.img.get_height()
          self.x = SCREEN_WIDTH//2 
@@ -48,7 +48,7 @@ class Player(object):
           self.rotated_rect.center = (self.x,self.y)
           self.cosine = math.cos(math.radians(self.angle + 90))
           self.sine = math.sin(math.radians(self.angle +90))
-          self.head = (self.x + self.cosine + self.width//2, self.y - self.sine * self.height/2)
+          self.head = (self.x + self.cosine * self.width//2, self.y - self.sine * self.height/2)
      
      def turn_right(self):
           self.angle -=5
@@ -57,77 +57,70 @@ class Player(object):
           self.rotated_rect.center = (self.x,self.y)
           self.cosine = math.cos(math.radians(self.angle + 90))
           self.sine = math.sin(math.radians(self.angle +90))
-          self.head = (self.x + self.cosine + self.width//2, self.y - self.sine * self.height/2)     
+          self.head = (self.x + self.cosine * self.width//2, self.y - self.sine * self.height/2)     
 
      def move_forward(self):
         self.x += self.cosine * 6
         self.y -= self.sine * 6
-        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
-        self.rotatedRect = self.rotatedSurf.get_rect()
-        self.rotatedRect.center = (self.x, self.y)
+        self.rotated_surface = pygame.transform.rotate(self.img, self.angle)
+        self.rotated_rect = self.rotated_surface.get_rect()
+        self.rotated_rect.center = (self.x, self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
         self.head = (self.x + self.cosine * self.width // 2, self.y - self.sine * self.height // 2)
-
-     def update_location(self):
+     
+     def update_location(self):  
           if self.x > SCREEN_WIDTH+ 50:
-            self.x = 0
+              self.x = 0
           elif self.x < 0 - self.width:
-            self.x = SCREEN_WIDTH
+              self.x = SCREEN_WIDTH
           elif self.y < -50:
-            self.y = SCREEN_HEIGHT
+              self.y = SCREEN_HEIGHT
           elif self.y > SCREEN_HEIGHT + 50:
-            self.y = 0 
+              self.y = 0 
 
 
 class Bullet(object):
      def __init__(self):
-          self.point = player.head 
+          self.point = player.head
           self.x, self.y = self.point
-          self.width = 4
-          self.height = 4
-          self.c = player.cosine 
-          self.s = player.sine 
+          # bullets are 4 pixels big
+          self.w = 4
+          self.h = 4
+          self.c = player.cosine
+          self.s = player.sine
           self.xv = self.c * 10
           self.yv = self.s * 10
-
      def move(self):
-        self.x += self.xv
-        self.y -= self.yv
+          self.x += self.xv
+          self.y -= self.yv
 
-     def draw(self, window):
-        pygame.draw.rect(window, (255, 255, 255), [self.x, self.y, self.width, self.height])
-     
+     def draw(self,window):
+          pygame.draw.rect(window, (255,255,255),[self.x,self.y,self.w,self.h])  
      def check_off_screen(self):
           if self.x < -50 or self.x > SCREEN_WIDTH or self.y > SCREEN_HEIGHT or self.y < -50:
-               return True    
-
-
-
-
-              
+                        return True    
 
 def redraw_window():
         window.blit(bg,(0,0))
         player.draw(window)
-
-        for b in bullets:
-             b.draw(window)
-             
+        for b in player_bullets:
+            b.draw(window)
 
         pygame.display.update()
 
 player = Player()
-bullets = []
+player_bullets = []
 # game loop
 while run:
     clock.tick(60)
     if not game_over: 
          player.update_location()
-         for b in bullets:
-              b.move() 
+         for b in player_bullets:
+              b.move()
               if b.check_off_screen():
-                  bullets.pop(bullets.index(b))
+                  player_bullets.pop(player_bullets.index(b))
+
 
          keys = pygame.key.get_pressed()
          if keys[pygame.K_LEFT]:
@@ -141,8 +134,8 @@ while run:
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    if not game_over:
-                         bullets.append(Bullet())
+             if event.key == pygame.K_SPACE:
+                 if not game_over: 
+                     player_bullets.append(Bullet())   
     redraw_window()
 pygame.quit()
