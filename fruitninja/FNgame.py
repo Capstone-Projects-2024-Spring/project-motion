04e1528efff54b92cac4c4901c01bce75a122590
Theodore Glenn <tup36873@temple.edu>
2024-04-01 23:41:23 -0400

    The beginning of fruit ninja game creation. Initialized some variables  and game window size.

---
+++ fruitninja/FNgame.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+
+#Creating dimensions of game window
+window_width = 800
+window_height = 800
+
+fruits = ['Watermelon', 'Mango', 'Pineapple', 
+          'Coconut', 'Strawberry', 'Green Apple', 
+          'Red Apple', 'Kiwi', 'Banana', 'Orange']
+current_score = 0 #starting score
+lives = 3 #total life of gamer
