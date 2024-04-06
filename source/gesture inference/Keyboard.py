6e05c9cd091308ee6e5ff90bc3acfb34936d78c0
JayLiuTU <tuj76761@temple.edu>
2024-04-05 22:17:33 -0400

    half refactored

--- source/gesture inference/Keyboard.py
+++ source/gesture inference/Keyboard.py
@@ -9,9 +9,8 @@ class Keyboard(threading.Thread):
     def __init__(
         self,
         threshold=0.0,
-        toggle_key_threshold=0.15,
-        toggle_key_toggle_time=1,
-        toggle_mouse_func=None,
+        toggle_threshold=0.3,
+        threshold_off_time=0.05,
         flags=None,
         tps=60,
     ) -> None:
@@ -21,25 +20,19 @@ class Keyboard(threading.Thread):
 
         Args:
             threshold (float, optional): don't press key if press will be less than this time. Defaults to 0.0.
-            toggle_key_threshold (float, optional): don't press the toggle key if press will be less than this time. Defaults to 0.15.
-            mouse_toggle_key (str, optional): the key to use as theh mosue ontrol toggle. . Defaults to 'm'.
-            toggle_key_toggle_time (int, optional): while mouse toggle is being continously recieved, wait this long before toggling mouse
+            toggle_threshold (float, optional): if same key is receieved twice within this time, but not continously, key will be toggled
         """
         pydirectinput.FAILSAFE = False
         pydirectinput.PAUSE = 0
         self.tps = tps
         self.threshold = threshold
-        self.toggle_key_threshold = toggle_key_threshold
-        self.last_time = time.time()
-        self.last_time_toggle_key = time.time()
-        self.last_key = ""
-        self.toggle_key_pressed = False
-        self.key_pressed = False
-        self.toggle_mouse_func = toggle_mouse_func
-        self.toggle_key_toggle_time = toggle_key_toggle_time
+        self.toggle_threshold = toggle_threshold
+        self.last_key = "none"
+        self.toggle_keys_pressed = {}
+        self.key_pressed = ("none", time.time())
         self.console = GestureConsole()
         self.flags = flags
-        # self.keys = ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', 'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace', 'browserback', 'browserfavorites', 'browserforward', 'browserhome', 'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear', 'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete', 'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20', 'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja', 'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail', 'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack', 'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn', 'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn', 'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator', 'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab', 'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen', 'command', 'option', 'optionleft', 'optionright']
+        self.threshold_off_time = threshold_off_time
 
     def run(self):
         event = threading.Event()
@@ -59,11 +52,14 @@ class Keyboard(threading.Thread):
             event.wait(timeout=1 / self.tps)
 
     def release(self):
-        if self.key_pressed:
-            pydirectinput.keyUp(self.last_key)
-            self.key_pressed = False
-        if self.toggle_key_pressed:
-            self.toggle_key_pressed = False
+        if self.key_pressed[0] != "none":
+            self.console.print(f"releasing key: {self.key_pressed[0]}")
+            pydirectinput.keyUp(self.key_pressed[0])
+        for key in self.toggle_keys_pressed:
+            self.console.print(f"releasing key: {key}")
+            pydirectinput.keyUp(key)
+        self.toggle_keys_pressed = {}
+        self.key_pressed = ("none", time.time())
 
     def gesture_input(self, confidences):
         max_value = np.max(confidences)
@@ -72,9 +68,9 @@ class Keyboard(threading.Thread):
         if max_value > self.flags["min_confidence"]:
             max_index = np.argmax(confidences)
             if max_index == 0:
-                self.press("space")
-            elif max_index == 1:
                 self.press("none")
+            elif max_index == 1:
+                self.press("space")
             elif max_index == 2:
                 self.press(self.flags["toggle_mouse_key"])
             elif max_index == 3:
@@ -82,41 +78,46 @@ class Keyboard(threading.Thread):
 
     def press(self, key: str):
         current_time = time.time()
-        if key == "none" and self.last_key == "":
+
+        # if key is same update time and return
+        if self.last_key == key:
+            self.key_pressed = (key, current_time)
             return
-        if key == "none" and self.last_key != "":
-            self.key_pressed = False
-            self.console.print(f"releasing key: {self.last_key}")
-            pydirectinput.keyUp(self.last_key)  # Release the last key
-            self.last_key = ""
-            return
-        if key != self.last_key:
-            self.key_pressed = False
-            self.toggle_key_pressed = False
-            self.last_key = key
 
-            if key == self.flags["toggle_mouse_key"]:
-                self.last_time_toggle_key = current_time
-            else:
-                self.last_time = current_time
+        # self.key_pressed = (key, current_time)
 
+        # disable toggle on this key if it is toggled
+        if key in self.toggle_keys_pressed and self.toggle_keys_pressed[key]:
+            self.console.print(f"releasing key (was toggled): {key}")
+            self.toggle_keys_pressed[key] = False
+            pydirectinput.keyUp(key)  # Release the last key
+
+        # enable toggle on this key if it has already been pressed, its being recieved twiced in under the threshold time, and wasn't recieved for at least the off time
         if (
-            key == self.flags["toggle_mouse_key"]
-            and self.flags["toggle_mouse_key"] != None
+            key == self.key_pressed[0]
+            and current_time - self.key_pressed[1] < self.toggle_threshold
+            and current_time - self.key_pressed[1] > self.threshold_off_time
         ):
-            self.handle_toggle_key(current_time)
-        elif current_time - self.last_time > self.threshold and not self.key_pressed:
-            self.key_pressed = True
-            self.console.print(f"pressing key: {key}")
-            pydirectinput.keyDown(key)
+            if key != "none":
+                self.toggle_keys_pressed[key] = True
+                self.console.print(f"pressing key (toggled): {key}")
+                pydirectinput.keyDown(key)
 
-    def handle_toggle_key(self, current_time):
+        # if key is not toggled
         if (
-            not self.toggle_key_pressed
-            or current_time - self.last_time_toggle_key > self.toggle_key_toggle_time
+            not key in self.toggle_keys_pressed
+            or key in self.toggle_keys_pressed
+            and self.toggle_keys_pressed[key] == False
         ):
-            if current_time - self.last_time_toggle_key > self.toggle_key_threshold:
-                if self.toggle_mouse_func is not None:
-                    self.toggle_mouse_func()
-                self.toggle_key_pressed = True
-                self.last_time_toggle_key = current_time
+            # if there is a key pressed
+            if self.key_pressed[0] != "none":
+                self.console.print(f"releasing key: {self.key_pressed[0]}")
+                pydirectinput.keyUp(self.key_pressed[0])
+                self.key_pressed = (key, current_time)
+            # press
+            elif key != "none":
+                self.console.print(f"pressing key: {key}")
+                self.key_pressed = (key, current_time)
+                pydirectinput.keyDown(key)
+
+        self.last_key = key
