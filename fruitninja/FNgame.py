e15477539d4504db5c903803f557f2b6781d99be
Theodore Glenn <tup36873@temple.edu>
2024-04-16 21:47:36 -0400

    Game backdrop now works

--- fruitninja/FNgame.py
+++ fruitninja/FNgame.py
@@ -9,22 +9,39 @@ pygame.init()
 window_width = 800
 window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption('Fruit Ninja Game')
-backdrop = pygame.image.load('fruitninja/pics/backdrop.png') #background/wallpaper
+window_title = pygame.display.set_caption('Fruit Ninja Game - Motion Capstone')
+backdrop = pygame.image.load('fruitninja/pics/backdrop.png').convert()
 
-# Main Game's Loop
-running = True
-while running:
-    for event in pygame.event.get(): #iterates through all pygame events
-        if event.type == pygame.QUIT: #closing game window event
-            running = False
-
-
+# Game Data
 fruits = ['Watermelon', 'Mango', 'Pineapple', 
           'Coconut', 'Strawberry', 'Green Apple', 
           'Red Apple', 'Kiwi', 'Banana', 'Orange']
-current_score = 0 #starting score
-lives = 3 #total life of gamer
+score = 0 
+lives = 3 
+stats = {} # dictionary for holding data of objects
+
+def main_menu_screen():
+    window.blit(backdrop, (0,0))
+
+
+
+
+# Main Game's Loop
+running = True #manages game loop
+game_end = True #terminates while loop when out of lives
+round_one = True #in true state only for beginning of game
+while running:
+    if round_one:
+        main_menu_screen()
+        round_one = False
+
+    for event in pygame.event.get(): #iterates through all pygame events
+        if event.type == pygame.QUIT: #closing game window event
+            running = False
+    
+    pygame.display.flip()
+
+
 
 pygame.quit() #terminates pygame
 sys.exit() #cleanly stops execution
