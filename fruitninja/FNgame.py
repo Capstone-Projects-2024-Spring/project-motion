af550fe597fd61aa4917f7c47ec5d86ccbd8e3e2
Theodore Glenn <tup36873@temple.edu>
2024-04-20 18:48:35 -0400

    Refactored entire game of script to an object-oriented model involving encapsulation, and added new functions for making throwable objects and game physics.

--- fruitninja/FNgame.py
+++ fruitninja/FNgame.py
@@ -1,102 +1,220 @@
 import pygame
-import sys
 import random
+import sys
+import os
 
-# Initialize Pygame
-pygame.init()
+# Setting current working directory to the directory this script is to handle relative paths correctly
+abspath = os.path.abspath(__file__)  # Get absolute path of current script
+dname = os.path.dirname(abspath)  # Extract directory part of absolute path
+os.chdir(dname)  # Change current working directory to the script directory
 
-# Pygame data & Creating Game Window
-clock = pygame.time.Clock()
-window_width = 800
-window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
-window.fill((0,0,0))
-window_title = pygame.display.set_caption('Fruit Ninja Game -- Motion Capstone Project Mini Game')
-backdrop = pygame.image.load('fruitninja/pics/backdrop.png').convert()
+# Color constants
+green = (50, 200, 50)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-# Game Data
-fruits = ['Watermelon', 'Mango', 'Pineapple', 
-          'Coconut', 'Strawberry', 'Green Apple', 
-          'Red Apple', 'Kiwi', 'Banana', 'Orange']
-score = 0 
-lives = 3 
-stats = {} # dictionary for holding game object data
+class GameFN:
+    """Main game class for Fruit Ninja style game."""
 
-# Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (50, 200, 50)
+    # Constructor
+    def __init__(self):
+        """Initializes the game, creates window, and loads resources."""
+        pygame.init()
+        self.window_width = 800
+        self.window_height = 600
+        self.window = pygame.display.set_mode((self.window_width, self.window_height))
+        pygame.display.set_caption("Fruit Ninja")
+        self.backdrop = pygame.image.load("pics/backdrop.png").convert()
+        self.window.fill((0, 0, 0))
+        self.fps = 14
+        self.timer = pygame.time.Clock()
 
-# Fonts & Text
-title_font = pygame.font.SysFont(None, 60)
-button_font = pygame.font.SysFont(None, 40)
-score_font = pygame.font.SysFont(None, 30)
-score_text = score_font.render('Score : ' + str(score), True, (255, 255, 255))
+        self.score = 0
+        self.strikes = 0
+        self.stats = {} # 2D dictionary of throwables and correlated stats
+        self.throwables = ["bomb", "orange", "strawberry", "watermelon", "redapple", "greenapple", "coconut", "banana"] # 1D list of throwables
+        self.init_objects()
 
+        self.title_font = pygame.font.SysFont(None, 60)
+        self.button_font = pygame.font.SysFont(None, 40)
+        self.score_font = pygame.font.SysFont(None, 30)
+        
+        self.running = True
+        self.in_menu = True
+        self.game_start = True
+        self.game_end = True
+    
+    def init_objects(self):
+        """Propogates the stats dictionary once"""
+        for obj in self.throwables:
+            self.make_throwables(obj)
+    
+    def main_menu(self):
+        """Displays the main menu with play and quit options."""
+        self.window.fill(green)
 
-def main_menu():
-    window.fill(GREEN)
+        # Draws the title
+        title_text = self.title_font.render("FRUIT NINJA", True, white)
+        title_rect = title_text.get_rect(center=(self.window_width/2, self.window_height/4))
+        self.window.blit(title_text, title_rect)
 
-    # Draws the title
-    title_text = title_font.render("FRUIT NINJA", True, WHITE)
-    title_rect = title_text.get_rect(center=(window_width/2, window_height/4))
-    window.blit(title_text, title_rect)
+        # Draws Play button
+        play_text = self.button_font.render("Play", True, white)
+        play_rect = play_text.get_rect(center=(self.window_width/2, self.window_height/2))
+        pygame.draw.rect(self.window, white, play_rect.inflate(10, 5), 1)
+        self.window.blit(play_text, play_rect)
 
-    # Draws the "Play" button
-    play_text = button_font.render("Play", True, WHITE)
-    play_rect = play_text.get_rect(center=(window_width/2, window_height/2))
-    pygame.draw.rect(window, WHITE, play_rect.inflate(10, 5), 1)
-    window.blit(play_text, play_rect)
+        # Draws Quit button
+        quit_text = self.button_font.render("Quit", True, white)
+        quit_rect = quit_text.get_rect(center=(self.window_width/2, self.window_height/1.5))
+        pygame.draw.rect(self.window, white, quit_rect.inflate(10, 5), 1)
+        self.window.blit(quit_text, quit_rect)
 
-    # Draws the "Quit" button
-    quit_text = button_font.render("Quit", True, WHITE)
-    quit_rect = quit_text.get_rect(center=(window_width/2, window_height/1.5))
-    pygame.draw.rect(window, WHITE, quit_rect.inflate(10, 5), 1)
-    window.blit(quit_text, quit_rect)
+        # Updates display
+        pygame.display.flip()
+        return play_rect, quit_rect
 
-    # Updates display
-    pygame.display.flip()
+    def restart_menu(self):
+        """Displays the game over screen and waits for the player to restart or quit."""
+        self.window.blit(self.backdrop, (0, 0)) # clears the screen
+        self.render_words("Game Over", 80, self.window_width/2-10, self.window_height/4, black)
+        self.render_words(f"Score: {self.score}", 100, self.window_width/2-3, self.window_height/2-7, green)
+        self.render_words("Press any key to restart", 70, self.window_width/2, self.window_height*3/4, white)
+        pygame.display.flip()
 
-    return play_rect, quit_rect
-
-
-
-running = True #manages game loop
-starting_game = True #in true state for beginning of game
-game_end = True #terminates while loop when out of lives
-in_menu = True #beginning game state
-# Main game Loop
-while running:
-
-    if starting_game:
-        play_rect, quit_rect = main_menu()
-        starting_game = False
-
-    for event in pygame.event.get(): #iterates through all pygame events
-        if event.type == pygame.QUIT: #closing game window event
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            if in_menu:
-                # Check if play button clicked
-                if play_rect.collidepoint(mouse_pos):
-                    in_menu = False
-                    # start game here
-                    print("starting")
-                    window.blit(backdrop, (0,0)) #inserts backdrop into game's window
-                    window.blit(score_text, (0,0)) #inserts score into top of window
-
-                # Check if quit button clicked
-                elif quit_rect.collidepoint(mouse_pos):
+        choosing = True
+        while choosing: #loops until player chooses an event
+            for event in pygame.event.get():
+                if event.type == pygame.KEYUP: #restart event
+                    choosing = False
+                if event.type == pygame.QUIT: #quit event
                     pygame.quit()
-                    sys.exit()
-    
 
+    def render_words(self, input, size, x, y, color):
+        """Helper function to render text on the screen."""
+        font = pygame.font.SysFont(None, size)  # Font object with default system font
+        surface = font.render(input, True, color)  # Render the text onto new surface with antialiasing
+        rect = surface.get_rect()  # Get rectangular area of surface which text is drawn onto
+        rect.midtop = (x, y)  # Set middle-top coordinate of text rectangle for positioning
+        self.window.blit(surface, rect)  # Draws text surface onto window at specified text rect position
 
-    
-    pygame.display.flip()
-    clock.tick(30) # loops game at 30 fps
+    def render_strikes(self, x, y, strikes, picture):
+        """Displays the number of strikes (lives lost) on the screen."""
+        for i in range(strikes):
+            pic = pygame.image.load(picture)
+            pic_rect = pic.get_rect()
+            pic_rect.x = int(x + 35 * i)
+            pic_rect.y = y
+            self.window.blit(pic, pic_rect)
 
+    def make_throwables(self, throwable):
+        """Creates and randomizes components of each throwable object."""
+        item_path = "pics/" + throwable + ".png"
+        # for key:value pairs in the stats dictonary (2D), the keys are throwables
+        # and the values are the dictionary of key:value pairs below
+        self.stats[throwable] = {
+            "x_pos": random.randint(50, self.window_width - 50),
+            "y_pos": self.window_height + 5,
+            "dx/dt": random.randint(-15, 15),
+            "dy/dt": random.randint(-68, -50),
+            "time": 0,
+            "pic": pygame.image.load(item_path),
+            "throwing": False,
+            "struck": False
+        }
+        if random.random() < 0.6:
+            self.stats[throwable]["throwing"] = False
+        else:
+            self.stats[throwable]["throwing"] = True
+
+    def make_physics(self):
+        """Handles the physics and interactions of all throwables."""
+        for key, value in self.stats.items():
+            if not value["throwing"]:
+                self.make_throwables(key)
+            else:
+                # Movement in different directions
+                value["x_pos"] += value["dx/dt"]
+                value["y_pos"] += value["dy/dt"]
+                value["dy/dt"] += (value["time"])
+                value["time"] += 1
+
+                # Check if throwable is within screen bounds
+                if value["y_pos"] > self.window_width:
+                    self.make_throwables(key)
+                else:
+                    self.window.blit(value["pic"], (value["x_pos"], value["y_pos"]))
+
+                # Collision detection with mouse click
+                cursor_pos = pygame.mouse.get_pos()
+
+                # Defined bounds of the object for clear collision detection
+                x_min = value["x_pos"]
+                x_max = value["x_pos"] + 60
+                y_min = value["y_pos"]
+                y_max = value["y_pos"] + 60
+
+                # Check if the cursor is within the bounds of the object
+                cursor_within_x_bounds = x_min < cursor_pos[0] < x_max
+                cursor_within_y_bounds = y_min < cursor_pos[1] < y_max
+
+                # Perform the check using simplified conditions
+                if not value["struck"] and cursor_within_x_bounds and cursor_within_y_bounds:                
+                    if key != "bomb": # when fruits are struck
+                        self.score += 1
+                        cut_path = "pics/cut_" + key + ".png"
+                    else: # when bombs are struck
+                        self.strikes += 1
+                        if self.strikes >= 3:
+                            self.game_end = True
+                            self.restart_menu()
+                        cut_path = "pics/explode.png"
+
+                    value["pic"] = pygame.image.load(cut_path)
+                    value["dx/dt"] += 20
+                    value["struck"] = True
+
+    def run(self):
+        """Main game loop."""
+        while self.running:
+            self.timer.tick(self.fps) #loops game at specified fps
+
+            if self.game_start: #starting state
+                play_rect, quit_rect = self.main_menu()
+                self.game_start = False
+
+            if self.game_end: #restart state
+                self.score = 0
+                self.strikes = 0
+                self.game_end = False
+
+            for event in pygame.event.get(): #iterates through all pygame events
+                if event.type == pygame.QUIT: #closing game window event
+                    self.running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if self.in_menu:
+                        # Check if play button clicked
+                        if play_rect.collidepoint(mouse_pos):
+                            self.in_menu = False
+                            
+                        # Check if quit button clicked
+                        elif quit_rect.collidepoint(mouse_pos):
+                            pygame.quit()
+                            sys.exit()
+
+            if not self.in_menu:
+                self.window.blit(self.backdrop, (0, 0))
+                self.render_words(f"Score: {self.score}", 40, 80, 0, green)
+                self.render_strikes(self.window_width - 130, 4, 3, "pics/emptystrike.png")
+                self.render_strikes(self.window_width - 130, 4, self.strikes, "pics/redstrike.png")
+                self.make_physics()
+
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = GameFN()
+    game.run()
 
 pygame.quit() #terminates pygame
 sys.exit() #cleanly stops execution
